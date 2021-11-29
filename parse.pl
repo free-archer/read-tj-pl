c000e01a64d93dc3420a9a63e7fbe408823d39d5
Беспалов Алексей <bespalov.alexey@yandex.ru>
2021-11-29 16:03:17 +0700

    Не удачные попытки разобрать хеш

--- parse.pl
+++ parse.pl
@@ -40,30 +40,7 @@ while (my $str = <$fh>) {
 
     if ($str =~ /([0-9]{2}:[0-9]{2}\.[0-9]+\-[0-9]+)\,(\w+)\,(\d+)/)
     {
-        my ($y,$m,$d,$h) = $filename =~ /^([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/ or die;
-        my ($mm,$ss) = $str =~ /([0-9]{2}):([0-9]{2})\./ or die;
-        my ($id,$event,$level) = $str =~ /([0-9]{2}:[0-9]{2}\.[0-9]+\-[0-9]+)\,(\w+)\,(\d+)/ or die;
-        my $datetime = DateTime->new(
-        year      => "20".$y,
-        month     => $m,
-        day       => $d,
-        hour       => $h,
-        minute     => $mm,
-        second     => $ss,        
-        time_zone => 'local',
-        );
-        #say $datetime;
 
-        my %main = (
-            "time"=>$datetime->datetime(),
-            "id"=>$id,
-            "event"=>$event,
-            "level"=>$level,
-        );
-        push(@properties, \%main);
-
-        #warn Dumper(@main);
-        # say scalar @main;
 
         if (length($str_log)) {
             push(@arr, $str_log);
@@ -81,7 +58,28 @@ say scalar @arr;
 
 #GET PROPERTIES
 for (my $i=0; $i<scalar @arr; $i++) {
-    my %tj;
+    my $str = $arr[$i];
+
+    my ($y,$m,$d,$h) = $filename =~ /^([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/ or die;
+    my ($mm,$ss) = $str =~ /([0-9]{2}):([0-9]{2})\./ or die;
+    my ($id,$event,$level) = $str =~ /([0-9]{2}:[0-9]{2}\.[0-9]+\-[0-9]+)\,(\w+)\,(\d+)/ or die;
+    my $datetime = DateTime->new(
+        year      => "20".$y,
+        month     => $m,
+        day       => $d,
+        hour       => $h,
+        minute     => $mm,
+        second     => $ss,
+        time_zone => 'local',
+    );
+
+    my %tj = (
+        "time"=>$datetime->datetime(),
+        "id"=>$id,
+        "event"=>$event,
+        "level"=>$level,
+    );
+
     while ($arr[$i] =~ m/,([A-Za-z0-9_А-Яа-я:]+)=([^,]+)/g) {
         unless (grep(/^$1$/, @columns)) {
             push(@columns, $1);
@@ -129,17 +127,30 @@ if (0) {
 #INSERT DATA INTO TABLE
 #my $sql_insert = "INSERT INTO $db_table (" . join(", ", @quote_columns) . " VALUES (" . join(", ", @properties[@_]) . ");";
 my $sql_insert = "INSERT INTO $db_table (";
-my $col;
-my $val;
+my $col = "";
+my $val = "";
 
-warn Dumper @columns;
-warn Dumper @properties;
+# my $tt = $properties[1];
+# warn Dumper $properties[0];
+print $properties[0];
+# say $tt->{"p:processName"};
+# warn Dumper $properties[1]->{SessionID};
+# warn Dumper $properties[1]->{"SessionID"};
 for (my $i=0; $i<scalar @columns; $i++) {
-    # $col = $col . "$columns[i]";
-    # $val = $val . "$properties->[i]->{$columns[i]}";
+    my $column = $columns[$i];
+    say $column;
+    $col = $col . "$quote_columns[$i], ";
+
+    my $prop = $properties[$i];
+    if (exists $prop->{$column}) {
+        say $prop->{$column};
+        $val = $val . ", $prop->{$column}";
+    }
+    say $val;
 
 }
-
+#warn Dumper $col;
+# warn Dumper $val;
 say $sql_insert;
 
 
